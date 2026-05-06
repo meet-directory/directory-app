161bb45a08ed4e108e1412b217305d476c8962a7
cixil <cecil@posteo.net>
2026-05-06 15:18:39 -0400

    restrict popup windows to screen size

--- app/popups/confirmation_popup_dialogue.gd
+++ app/popups/confirmation_popup_dialogue.gd
@@ -4,6 +4,8 @@ class_name ConfirmationPopup
 @onready var description_label: Label = %DescriptionLabel
 @onready var cancel_button: Button = %CancelButton
 @onready var confirm_button: Button = %ConfirmButton
+@onready var popup: MarginContainer = %Popup
+@onready var scroll_content: Label = %DescriptionLabel
 
 signal confirm_pressed
 signal cancel_pressed
@@ -11,14 +13,25 @@ signal cancel_pressed
 const INFO_CHAR = 'ℹ️'
 
 func _ready() -> void:
-	var screen_size = get_viewport().get_visible_rect().size
-	description_label.custom_minimum_size.x = screen_size.x/2
+	#_resize()
 	cancel_button.grab_focus()
+	
+func _resize() -> void:
+	var screen_size = get_viewport().get_visible_rect().size
+	popup.custom_minimum_size = screen_size/2
+	await get_tree().process_frame
+	if scroll_content.size.y > popup.custom_minimum_size.y:
+		popup.custom_minimum_size.y = min(screen_size.y, scroll_content.size.y + cancel_button.size.y)
+	for i in range(1, 4):
+		await get_tree().process_frame
+		if scroll_content.size.y > popup.custom_minimum_size.y:
+			popup.custom_minimum_size.x = min(screen_size.x, scroll_content.size.x + 50*i)
 
 func set_text(text:String, cancel_btn:String="Go back", confirm_btn="Confirm"):
 	description_label.text = INFO_CHAR + ' ' + text
 	cancel_button.text = cancel_btn
 	confirm_button.text = confirm_btn
+	_resize()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
--- app/popups/info_popup_dialogue.gd
+++ app/popups/info_popup_dialogue.gd
@@ -1,18 +1,32 @@
 extends CanvasLayer
 class_name InfoPopup
+@onready var popup: MarginContainer = %Popup
 
 signal closed
 
 @onready var description_label: Label = %DescriptionLabel
 @onready var close_button: Button = %CloseButton
+@onready var scroll_content: VBoxContainer = %ScrollContent
 
 func _ready() -> void:
-	var screen_size = get_viewport().get_visible_rect().size
-	description_label.custom_minimum_size.x = screen_size.x/2
+	#_resize()
 	close_button.grab_focus()
+
+func _resize() -> void:
+	var screen_size = get_viewport().get_visible_rect().size
+	popup.custom_minimum_size = screen_size/2
+	await get_tree().process_frame
+	if scroll_content.size.y > popup.custom_minimum_size.y:
+		popup.custom_minimum_size.y = min(screen_size.y, scroll_content.size.y + close_button.size.y)
+	for i in range(1, 4):
+		await get_tree().process_frame
+		if scroll_content.size.y > popup.custom_minimum_size.y:
+			popup.custom_minimum_size.x = min(screen_size.x, scroll_content.size.x + 50*i)
 
 func set_text(text:String):
 	description_label.text = text
+	await get_tree().process_frame
+	_resize()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
